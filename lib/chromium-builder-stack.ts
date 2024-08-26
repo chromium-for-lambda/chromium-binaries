8b69ef8b2b37889164844d54160652d2babc4fc0
Stephan Kaag <stephan@ka.ag>
2024-08-26 15:58:30 +0200

    Update chromium-builder-stack.ts

--- lib/chromium-builder-stack.ts
+++ lib/chromium-builder-stack.ts
@@ -10,7 +10,6 @@ const installCmakeCommand = RunnerImageComponent.custom({
   name: "InstallCMake",
   commands: [
     'yum install -y gcc10 gcc10-c++ zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl11 openssl11-devel xz xz-devel libffi-devel',
-    'yum remove -y gcc',
     'export CC="/bin/gcc10-cc"',
     'export CXX="/bin/gcc10-c++"',
     'yum install -y wget tar',
@@ -29,7 +28,8 @@ const installCmakeCommand = RunnerImageComponent.custom({
 const installAL2BuildDependencies = RunnerImageComponent.custom({
   name: "InstallAL2BuildDependencies",
   commands: [
-    'yum install -y "@Development Tools" nano git alsa-lib-devel atk-devel bc bluez-libs-devel bzip2-devel cairo-devel cups-devel dbus-devel dbus-glib-devel dbus-x11 expat-devel glibc-langpack-en gperf gtk3-devel httpd libatomic libcap-devel libjpeg-devel libXScrnSaver-devel libxkbcommon-x11-devel mod_ssl ncurses-devel ncurses-compat-libs nspr-devel nss-devel pam-devel pciutils-devel perl php php-cli pulseaudio-libs-devel ruby xorg-x11-server-Xvfb libcurl-devel libxml2-devel clang libdrm-devel libuuid-devel mesa-* --skip-broken'
+    'yum install -y "@Development Tools" nano git alsa-lib-devel atk-devel bc bluez-libs-devel bzip2-devel cairo-devel cups-devel dbus-devel dbus-glib-devel dbus-x11 expat-devel glibc-langpack-en gperf gtk3-devel httpd libatomic libcap-devel libjpeg-devel libXScrnSaver-devel libxkbcommon-x11-devel mod_ssl ncurses-devel ncurses-compat-libs nspr-devel nss-devel pam-devel pciutils-devel perl php php-cli pulseaudio-libs-devel ruby xorg-x11-server-Xvfb libcurl-devel libxml2-devel clang libdrm-devel libuuid-devel mesa-* --skip-broken',
+    'yum remove -y gcc'
   ]
 })
 
